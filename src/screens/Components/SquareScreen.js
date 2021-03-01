66b24a206dfd57462127f5d2d5497516e4cf16a8
keaton <keatonrgallagher@gmail.com>
2021-03-01 15:01:18 -0700

    Refactored to follow reducer conventions and deconstructed state with in the component

--- src/screens/Components/SquareScreen.js
+++ src/screens/Components/SquareScreen.js
@@ -18,14 +18,14 @@ const COLOR_INCREMENT = 10;
 const reducer = (state, action) => { // Function outside of component so we don't get confused with state value
     // state = { red: number, green: number, blue: number }
     // action = { colorToChange: 'red' || 'green' ||  'blue', amount: 15 || -15 }  
-    switch (action.colorToChange) {
+    switch (action.type) {
         // Never going to change state directly
         case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : { ...state, red: state.red + action.amount }
+            return state.red + action.payload > 255 || state.red + action.payload < 0 ? state : { ...state, red: state.red + action.payload }
         case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : { ...state, green: state.green + action.amount }
+            return state.green + action.payload > 255 || state.green + action.payload < 0 ? state : { ...state, green: state.green + action.payload }
         case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : { ...state, blue: state.blue + action.amount }
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 ? state : { ...state, blue: state.blue + action.payload }
         default:
             return state; // Always returning the state when using reducer
     }
@@ -34,28 +34,28 @@ const reducer = (state, action) => { // Function outside of component so we don'
 const SquareScreen = () => {
     const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
     // Dispatch - running the reducer
-
+    const { red, green, blue } = state
     return (
 
-        <View style={styles.container}>
+        <View style={styles.container} >
             <ColorCounter
-                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'red', payload: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'red', payload: -1 * COLOR_INCREMENT })}
                 color="Red"
             />
             <ColorCounter
-                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'green', payload: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'green', payload: -1 * COLOR_INCREMENT })}
                 color="Green"
             />
             <ColorCounter
-                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'blue', payload: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'blue', payload: -1 * COLOR_INCREMENT })}
                 color="Blue"
             />
-            <Text style={styles.text}>RGB: Red: {state.red}, Green: {state.green}, Blue: {state.blue}</Text>
-            <View style={{ marginTop: 25, height: 200, width: 200, backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})` }} />
-        </View>
+            <Text style={styles.text}>RGB: Red: {red}, Green: {green}, Blue: {blue}</Text>
+            <View style={{ marginTop: 25, height: 200, width: 200, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+        </ View>
     )
 }
 
